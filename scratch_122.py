16c2d6a51573057bf02cd0b708941e00174744d3
arjmig <arjmig98@gmail.com>
2023-04-20 15:58:18 +0200

    In this version it goes right and i got a DataSeries with mean fallrain for every province

--- scratch_122.py
+++ scratch_122.py
@@ -2,11 +2,10 @@
 import requests
 import pandas as pd
 
-fechaIniStr = '2021-04-01T00:00:00'
-fechaFinStr = '2021-05-01T00:00:00'
-idema = '0252D'
+fechaIniStr = '2023-04-01T00:00:00UTC'
+fechaFinStr = '2023-05-01T00:00:00UTC'
 url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/" \
-      f"fechaini/{fechaIniStr}/fechafin/{fechaFinStr}/estacion/{idema}"
+      f"fechaini/{fechaIniStr}/fechafin/{fechaFinStr}/todasestaciones"
 
 with open('token_1.txt', 'r') as file:
     api_key = file.read()
@@ -17,4 +16,27 @@ headers = {
     'cache-control': "no-cache"
     }
 
-response = requests.request('GET', url, headers=headers, params=querystring).text
+response = requests.request('GET', url, headers=headers, params=querystring).json()
+stations_data = requests.request('GET', response['datos'], headers=headers, params=querystring).json()
+stations_data = pd.DataFrame(stations_data)
+
+##
+def to_float(x):
+    if type(x) == str:
+        if not(',' in x):
+            return 0
+        a = list(x)
+        for n in range(len(x)):
+            if a[n] == ',':
+                a[n] = '.'
+        return float(''.join(a))
+    else:
+        return 0
+
+##
+stations_data['prec'] = stations_data.prec.apply(to_float)
+##
+mean_prec = stations_data.groupby('provincia')['prec'].mean()
+
+##
+
