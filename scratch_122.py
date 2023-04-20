3053533e93c2c001fb38f6f6b95667a4ddd7149a
arjmig <arjmig98@gmail.com>
2023-04-20 17:56:24 +0200

    It is a possible version in which I try to take all aprils from 2000 to 2023 and concatenate them in a single DataFrame

--- scratch_122.py
+++ scratch_122.py
@@ -2,28 +2,17 @@
 import requests
 import pandas as pd
 
-fechaIniStr = '2023-04-01T00:00:00UTC'
-fechaFinStr = '2023-05-01T00:00:00UTC'
-url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/" \
-      f"fechaini/{fechaIniStr}/fechafin/{fechaFinStr}/todasestaciones"
-
 with open('token_1.txt', 'r') as file:
     api_key = file.read()
-
 querystring = {"api_key": api_key}
-
 headers = {
     'cache-control': "no-cache"
     }
 
-response = requests.request('GET', url, headers=headers, params=querystring).json()
-stations_data = requests.request('GET', response['datos'], headers=headers, params=querystring).json()
-stations_data = pd.DataFrame(stations_data)
 
-##
 def to_float(x):
     if type(x) == str:
-        if not(',' in x):
+        if not (',' in x):
             return 0
         a = list(x)
         for n in range(len(x)):
@@ -33,10 +22,20 @@ def to_float(x):
     else:
         return 0
 
-##
-stations_data['prec'] = stations_data.prec.apply(to_float)
-##
-mean_prec = stations_data.groupby('provincia')['prec'].mean()
+all_aprils = pd.Series()
 
-##
+for n in range(2000, 2023):
+    fechaIniStr = f'{n}-04-01T00:00:00UTC'
+    fechaFinStr = f'{n}-05-01T00:00:00UTC'
+    url = "https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/" \
+        f"fechaini/{fechaIniStr}/fechafin/{fechaFinStr}/todasestaciones"
+    response = requests.request('GET', url, headers=headers, params=querystring).json()
+    stations_data = requests.request('GET', response['datos'], headers=headers, params=querystring).json()
+    stations_data = pd.DataFrame(stations_data)
+
+    stations_data['prec'] = stations_data.prec.apply(to_float)
+    mean_prec = stations_data.groupby('provincia')['prec'].mean()
+    pd.concat([all_aprils, mean_prec])
+
+
 
