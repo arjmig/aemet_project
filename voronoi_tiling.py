bb43dcd633e185780f7859e4ae142f8bc325aa1c
arjmig <arjmig98@gmail.com>
2023-05-03 11:52:01 +0200

    Finally all regions are plotted

--- voronoi_tiling.py
+++ voronoi_tiling.py
@@ -10,7 +10,8 @@ from scipy.spatial import Voronoi
 spain = gpd.read_file('https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_ESP_0.json', encoding='utf-8').explode(index_parts=True).geometry[0]
 spain = spain.to_crs(epsg=32630)
 remaining_spain = gpd.GeoDataFrame(spain[(spain.index >= 29) + (spain.index.isin([18, 19]))])
-canarias_isles = gpd.GeoDataFrame(spain)
+canarias_isles = gpd.GeoDataFrame(spain.iloc[:18])
+
 with open('stations_data.csv', 'r') as file:
     stations_data = pd.read_csv(file)
 stations_data = stations_data[['latitude', 'longitude', 'zone']]
@@ -39,12 +40,14 @@ for zone in [zone_1, zone_2]:
         d += [[point.x, point.y]]
     c += [d]
 
-np_zone_1 = np.array(c[0])
-np_zone_2 = np.array(c[1])
+bounderies_map_1 = np.array([[-47000, 4889000], [-32000, 3878000], [1150000, 3875000]])
+bounderies_map_2 = np.array([[-1023500, 3305500], [-500300, 3306600], [-483300, 3128800]])
+np_zone_1 = np.concatenate((np.array(c[0]), bounderies_map_1))
+np_zone_2 = np.concatenate((np.array(c[1]), bounderies_map_2))
 
 vor = Voronoi(np_zone_1)
+polygons = [Polygon(vor.vertices[region]) for region in vor.regions if not -1 in region]
 
-polygons = [Polygon(vor.vertices[region]) for region in vor.regions if -1 not in region]
 polygons = gpd.GeoDataFrame(geometry=polygons, crs=32630)
 polygons = polygons.overlay(remaining_spain, how='intersection')
 polygons = polygons.overlay(remaining_spain, how='union')
@@ -54,3 +57,14 @@ polygons.plot(ax=ax, markersize=3.5, edgecolor='black')
 zone_1.plot(ax=ax, markersize=3.5, color='red')
 plt.show()
 
+vor = Voronoi(np_zone_2)
+
+polygons = [Polygon(vor.vertices[region]) for region in vor.regions if -1 not in region]
+polygons = gpd.GeoDataFrame(geometry=polygons, crs=32630)
+polygons = polygons.overlay(canarias_isles, how='intersection')
+polygons = polygons.overlay(canarias_isles, how='union')
+
+fig, ax = plt.subplots()
+polygons.plot(ax=ax, markersize=3.5, edgecolor='black')
+zone_2.plot(ax=ax, markersize=3.5, color='red')
+plt.show()
